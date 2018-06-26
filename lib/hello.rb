14713db8de626d93da7a9bbe78587df9778a92b3
Jon Schulman <schulman99@gmail.com>
2018-06-26 14:59:56 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -9,12 +9,12 @@ if block_given?
 else
   puts "Hey! No block was given!"
 end
-# end
+end
 
 # call your method here!
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
